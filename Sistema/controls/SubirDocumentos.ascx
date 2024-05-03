434a17ed7b82d72310c499e2d1b831964f59d2c0
3GATO3 <106358166+Wilbert-Miau@users.noreply.github.com>
2024-05-03 17:39:52 -0600

    feat: upload documents

---
+++ Sistema/controls/SubirDocumentos.ascx
@@ -0,0 +1,58 @@
+﻿<%@ Control Language="vb" AutoEventWireup="false" CodeBehind="SubirDocumentos.ascx.vb" Inherits="Sistema.SubirDocumentos" %>
+
+<%--<button type="button" class="btn btn-primary p-5" data-toggle="modal" data-target="#myModal">
+  Agregar documento
+</button>--%>
+
+<form id="form1" method="post" enctype="multipart/form-data">
+    <div class="container p-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title text-center mb-4">
+                            Agregar documento
+                        </div>
+
+                        <div class="form-group">
+                            <asp:FileUpload ID="File1" CssClass="form-control-file" runat="server" />
+                        </div>
+                        <div class="text-center">
+                            <asp:Button class="btn btn-primary" ID="UploadDocuments" runat="server" Text="Subir" />
+                        </div>
+                        <div class="text-center mt-2">
+                            <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
+                            </
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+</form>
+
+<%--<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Agregar documentos</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+       <form id="form1" method="post" enctype="multipart/form-data">
+          <div class="form-group">
+            <asp:FileUpload ID="File1" CssClass="form-control-file" runat="server" />
+          </div>
+          <div class="text-center">
+            <asp:Button class="btn btn-primary" ID="UploadDocuments" runat="server" Text="Subir" />
+          </div>
+          <div class="text-center mt-2">
+            <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>--%>
---
+++ Sistema/controls/SubirDocumentos.ascx.vb
@@ -0,0 +1,64 @@
+﻿Imports System.IO
+
+Public Class SubirDocumentos
+    Inherits System.Web.UI.UserControl
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+    Protected Sub UploadDocumentsButton_Click(sender As Object, e As EventArgs) Handles UploadDocuments.Click
+        If Not File1.PostedFile Is Nothing And File1.PostedFile.ContentLength > 0 Then
+            ' Get file details
+            Dim Usuario = Session("Usuario")
+            Dim Clave = Session("Clave")
+            Dim Servidor = Session("Servidor")
+            Dim Bd = Session("Bd")
+            Dim fileName As String = File1.PostedFile.FileName
+            Dim fileSize As Long = File1.PostedFile.ContentLength
+            Dim conf As New Configuracion(Usuario, Clave, "FUNAMOR", Servidor)
+            Dim queryDocumentos As String
+            Dim numeroDeEmpleado As String
+            Dim relativePath As String = "Uploads/"
+            Dim path As String = Server.MapPath(relativePath)
+            Dim savePath As String = path & fileName
+            Dim completeRelativePath As String = relativePath & fileName
+            Dim descripcion As String
+            If Not Directory.Exists(path) Then
+                Directory.CreateDirectory(path)
+            End If
+            If fileSize > 21485760 Then ' 
+                lblMessage.Text = "Error: El archivo no puede tener un tamaño mayor a 20MB."
+                Exit Sub
+            End If
+            If Session("Codigo_Empleado") IsNot Nothing Then
+                Try
+
+                    numeroDeEmpleado = Session("Codigo_Empleado")
+                    File1.PostedFile.SaveAs(savePath)
+                    descripcion = "Contrato"
+                    queryDocumentos = " INSERT INTO [dbo].[DocumentosDeEmpleado]
+                    (
+                        [NumeroDeEmpleado],
+                        [NombreDelArchivo],
+                        [Ruta],
+                        [Descripcion]
+                    )
+                VALUES
+                    ('" + numeroDeEmpleado + "',
+                    '" + fileName + "',
+                    '" + completeRelativePath + "',
+                    '" + descripcion + "')"
+                    Dim Datos = conf.EjecutaSql(queryDocumentos)
+                    lblMessage.Text = "¡Carga exitosa!"
+
+                Catch ex As Exception
+                    lblMessage.Text = "Error al cargar archivos: " & ex.Message
+                End Try
+            Else
+                lblMessage.Text = "Error: No se pudo identificar el empleado para subir archivos." ' Inform user about missing session variable
+            End If
+        Else
+                lblMessage.Text = "Por favor seleccione un archivo para subir."
+        End If
+    End Sub
+End Class
