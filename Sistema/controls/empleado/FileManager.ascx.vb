61bce375d105c8cc4695b471cb246ea987f997ae
3GATO3 <106358166+Wilbert-Miau@users.noreply.github.com>
2024-05-18 15:37:48 -0600

    feat: enhanced handling error of upload docs

--- Sistema/controls/empleado/FileManager.ascx.vb
+++ Sistema/controls/empleado/FileManager.ascx.vb
@@ -10,13 +10,15 @@ Public Class FileManager
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         lblUploadMessage.ForeColor = Drawing.Color.Red
         lblUploadMessage.Text = "Archivo y descripción son obligatorios"
+
         If Session("Codigo_Empleado") Then
 
             Try
                 BindGridView()
                 lblMessage.Text = "Archivos encontrados: " & $"{MyGridView.Rows.Count}"
             Catch ex As Exception
-                lblMessage.Text = "Error al leer de la base de datos, por favor recargue la página : " & ex.Message
+                Dim msg = "Error al leer de la base de datos, por favor recargue la página : " & ex.Message
+                RaiseEvent AlertGenerated(Me, New AlertEventArgs(msg, "danger"))
             End Try
 
             If Not IsPostBack Then
@@ -28,39 +30,44 @@ Public Class FileManager
     End Sub
 
     Protected Sub MyGridView_RowCommand(ByVal sender As Object, ByVal e As GridViewCommandEventArgs)
+
         If e.CommandName = "DownloadFile" Then
-            Dim args As String() = e.CommandArgument.ToString().Split("|"c)
-            Dim rowIndex As Integer = Convert.ToInt32(e.CommandArgument)
 
-            Dim dataSource As DataTable = DirectCast(MyGridView.DataSource, DataTable)
+            Try
+                Dim args As String() = e.CommandArgument.ToString().Split("|"c)
+                Dim rowIndex As Integer = Convert.ToInt32(e.CommandArgument)
+                Dim dataSource As DataTable = DirectCast(MyGridView.DataSource, DataTable)
+                Dim documentPath As String = dataSource.Rows(rowIndex).Item("Ruta").ToString()
+                Dim documentName As String = dataSource.Rows(rowIndex).Item("NombreDelArchivo").ToString()
+                Dim handlerUrl As String = $"/Handlers/DownloadHandler.ashx?path={HttpUtility.UrlEncode(documentPath)}&name={HttpUtility.UrlEncode(documentName)}"
+                Response.Redirect(handlerUrl)
+            Catch ex As Exception
+                Dim msg = "Error de descarga: " & ex.Message
+                RaiseEvent AlertGenerated(Me, New AlertEventArgs(msg, "danger"))
+            End Try
 
-            Dim documentPath As String = dataSource.Rows(rowIndex).Item("Ruta").ToString()
-
-            Dim documentName As String = dataSource.Rows(rowIndex).Item("NombreDelArchivo").ToString()
-            Dim handlerUrl As String = $"/Handlers/DownloadHandler.ashx?path={HttpUtility.UrlEncode(documentPath)}&name={HttpUtility.UrlEncode(documentName)}"
-            Response.Redirect(handlerUrl)
         End If
+
     End Sub
     Protected Sub MyGridView_RowDeleting(ByVal sender As Object, ByVal e As GridViewDeleteEventArgs) Handles MyGridView.RowDeleting
-        Dim rowIndex As Integer = e.RowIndex
-        Dim id As String = MyGridView.DataKeys(rowIndex).Value.ToString()
-        Dim dataSource As DataTable = DirectCast(MyGridView.DataSource, DataTable)
-        Dim relativePath As String = dataSource.Rows(rowIndex).Item("Ruta").ToString()
-        Dim filePath As String = Server.MapPath(relativePath)
-
 
         Try
+            Dim rowIndex As Integer = e.RowIndex
+            Dim id As String = MyGridView.DataKeys(rowIndex).Value.ToString()
+            Dim dataSource As DataTable = DirectCast(MyGridView.DataSource, DataTable)
+            Dim relativePath As String = dataSource.Rows(rowIndex).Item("Ruta").ToString()
+            Dim filePath As String = Server.MapPath(relativePath)
+
             If FileHelper.DeleteFile(filePath) Then
-                DeleteRecordFromDatabase(id)
+                DeleteRecordFromDatabase(ID)
                 RaiseEvent AlertGenerated(Me, New AlertEventArgs("Archivo eliminado correctamente", "success"))
                 BindGridView()
-
             Else
                 RaiseEvent AlertGenerated(Me, New AlertEventArgs("Error al eliminar archivo", "danger"))
             End If
 
         Catch ex As Exception
-
+            RaiseEvent AlertGenerated(Me, New AlertEventArgs("Error de borrado: " & ex.Message, "danger"))
         End Try
 
 
@@ -71,14 +78,13 @@ Public Class FileManager
         Dim Servidor = Session("Servidor")
         Dim Bd = Session("Bd")
         Dim conf As New Configuracion(Usuario, Clave, "FUNAMOR", Servidor)
+
         Try
             queryDelete = " DELETE FROM [dbo].[DocumentosDeEmpleado] WHERE Id = " & id
             conf.EjecutaSql(queryDelete)
         Catch ex As Exception
             RaiseEvent AlertGenerated(Me, New AlertEventArgs("Error al eliminar registro de la base de datos, por favor vuelva a intentarlo : " & ex.Message, "danger"))
-
         End Try
-
 
     End Sub
 
@@ -90,26 +96,29 @@ Public Class FileManager
         Dim Servidor = Session("Servidor")
         Dim Bd = Session("Bd")
         Dim conf As New Configuracion(Usuario, Clave, "FUNAMOR", Servidor)
+
         Try
+
             If Session("Codigo_Empleado") Then
                 numeroDeEmpleado = Session("Codigo_Empleado")
                 queryRetrieve = " SELECT * FROM [dbo].[DocumentosDeEmpleado] WHERE NumeroDeEmpleado = " & numeroDeEmpleado
                 Dim Datos = conf.EjecutaSql(queryRetrieve)
                 MyGridView.DataSource = Datos.Tables(0)
                 MyGridView.DataBind()
+
                 If MyGridView.Rows.Count = 0 Then
                     lblMessage.Text = "No se encontraron documentos"
                 Else
                     lblMessage.Text = ""
                 End If
+
             Else
+
             End If
 
-
-
         Catch ex As Exception
-            lblMessage.Text = "Error, por favor vuelva a intentarlo : " & ex.Message
-
+            Dim msg = "Error, por favor vuelva a intentarlo : " & ex.Message
+            RaiseEvent AlertGenerated(Me, New AlertEventArgs(msg, "danger"))
         End Try
 
 
@@ -117,12 +126,15 @@ Public Class FileManager
     End Sub
 
     Protected Sub UploadFileButton_Click(sender As Object, e As EventArgs) Handles UploadFile.Click
+
         Try
             Dim Usuario = Session("Usuario")
             Dim Clave = Session("Clave")
             Dim Servidor = Session("Servidor")
             Dim Bd = Session("Bd")
+
             If Usuario IsNot Nothing AndAlso Clave IsNot Nothing AndAlso Servidor IsNot Nothing AndAlso Bd IsNot Nothing Then
+
                 If Not File1.PostedFile Is Nothing And Page.IsValid Then
                     Dim conf As New Configuracion(Usuario, Clave, "FUNAMOR", Servidor)
                     Dim fileName As String = File1.PostedFile.FileName
@@ -135,30 +147,33 @@ Public Class FileManager
                     If Not Directory.Exists(path) Then
                         Directory.CreateDirectory(path)
                     End If
+
                     If File1.PostedFile.ContentLength < 1 Then
                         RaiseEvent AlertGenerated(Me, New AlertEventArgs("Seleccione un archivo por favor.", "danger"))
-
                         Exit Sub
                     End If
+
                     If Not FileHelper.LessThanFileSizeLimit(fileSize, 10485760) Then
                         Dim msg As String = "El archivo no puede tener un tamaño mayor a 10MB"
                         Dim alertType As String = "danger"
                         RaiseEvent AlertGenerated(Me, New AlertEventArgs(msg, alertType))
                         Exit Sub
                     End If
+
                     If Not FileHelper.ValidateFileExtension(fileName, {".doc", ".docx", ".pdf", ".xls", ".xlsx", ".png", ".jpg", ".jpeg", ".odt", ".zip", ".rar", ".7z", ".mp4"}) Then
                         Dim msg As String = "Solo se admiten documentos, archivos comprimidos, fotos o videos cortos"
                         Dim alertType As String = "danger"
                         RaiseEvent AlertGenerated(Me, New AlertEventArgs(msg, alertType))
                         Exit Sub
                     End If
+
                     If TextBoxDescription.Text.Length < 1 Then
-
                         RaiseEvent AlertGenerated(Me, New AlertEventArgs("La descripción es obligatoria.", "danger"))
-
                         Exit Sub
                     End If
+
                     If Session("Codigo_Empleado") IsNot Nothing Then
+
                         Try
                             numeroDeEmpleado = Session("Codigo_Empleado")
                             fileName = numeroDeEmpleado & "_" & DateString & "_" & fileName
@@ -167,13 +182,11 @@ Public Class FileManager
                             Dim descripcion As String
                             descripcion = TextBoxDescription.Text
 
-
                             If FileHelper.CheckFileExists(savePath) Then
                                 RaiseEvent AlertGenerated(Me, New AlertEventArgs("Ya existe un archivo con ese nombre", "danger"))
                                 Exit Sub
                             Else
                                 File1.PostedFile.SaveAs(savePath)
-
                                 queryDocumentos = " INSERT INTO [dbo].[DocumentosDeEmpleado]
                                                     (
                                                         [NumeroDeEmpleado], [NombreDelArchivo],
@@ -185,26 +198,21 @@ Public Class FileManager
                                                         '" + completeRelativePath + "', '" + descripcion + "'
                                                     )"
 
-
                                 Dim Datos = conf.EjecutaSql(queryDocumentos)
                                 Dim msg As String = "Carga exitosa"
                                 Dim alertType As String = "success"
                                 RaiseEvent AlertGenerated(Me, New AlertEventArgs(msg, alertType))
-
                                 BindGridView()
                             End If
-
-
 
                         Catch ex As Exception
                             Dim msg = "Error al cargar archivos: " & ex.Message
                             RaiseEvent AlertGenerated(Me, New AlertEventArgs(msg, "danger"))
-
                         End Try
+
                     Else
                         Dim msg = "Error: No se pudo identificar el empleado para subir archivos."
                         RaiseEvent AlertGenerated(Me, New AlertEventArgs(msg, "danger"))
-
                     End If
 
                 Else
@@ -216,49 +224,56 @@ Public Class FileManager
         Catch ex As Exception
             Dim msg = "Error" & ex.Message
             RaiseEvent AlertGenerated(Me, New AlertEventArgs(msg, "danger"))
-
         End Try
+
     End Sub
 
     Protected Sub MyGridView_RowDataBound(ByVal sender As Object, ByVal e As GridViewRowEventArgs) Handles MyGridView.RowDataBound
-        If e.Row.RowType = DataControlRowType.DataRow Then
-            Dim sizeColumnIndex As Integer = 2
-            Dim fileName As String = e.Row.Cells(sizeColumnIndex).Text
-            Dim fileExtension As String = Path.GetExtension(fileName)
 
-            Dim iconCell As TableCell = e.Row.Cells(3)
-            Dim nameCell As TableCell = e.Row.Cells(2)
-            Dim imageClass = "bi bi-image"
-            Select Case fileExtension
-                Case ".docx"
-                    iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-file-word-fill text-primary""></i>"))
-                Case ".xls"
-                    iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-file-earmark-excel-fill text-success""></i>"))
-                Case ".xlsx"
-                    iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-file-earmark-excel-fill text-success""></i>"))
-                Case ".pdf"
-                    iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-file-earmark-pdf-fill text-danger""></i>"))
-                Case ".jpg"
-                    iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-image""></i>"))
-                Case ".jpg"
-                    iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-image""></i>"))
-                Case ".jpeg"
-                    iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-image""></i>"))
-                Case ".png"
-                    iconCell.Controls.Add(New LiteralControl($"<i class=""bi bi-image""></i>"))
-                Case Else
-                    iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-file-earmark-text-fill""></i>"))
-            End Select
+        Try
 
-            If fileName.Length > 32 Then
-                fileName = fileName.Substring(Math.Max(fileName.Length - 32, 0))
+            If e.Row.RowType = DataControlRowType.DataRow Then
+                Dim sizeColumnIndex As Integer = 2
+                Dim fileName As String = e.Row.Cells(sizeColumnIndex).Text
+                Dim fileExtension As String = Path.GetExtension(fileName)
+                Dim iconCell As TableCell = e.Row.Cells(3)
+                Dim nameCell As TableCell = e.Row.Cells(2)
+                Dim imageClass = "bi bi-image"
+
+                Select Case fileExtension
+                    Case ".docx"
+                        iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-file-word-fill text-primary""></i>"))
+                    Case ".xls"
+                        iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-file-earmark-excel-fill text-success""></i>"))
+                    Case ".xlsx"
+                        iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-file-earmark-excel-fill text-success""></i>"))
+                    Case ".pdf"
+                        iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-file-earmark-pdf-fill text-danger""></i>"))
+                    Case ".jpg"
+                        iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-image""></i>"))
+                    Case ".jpg"
+                        iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-image""></i>"))
+                    Case ".jpeg"
+                        iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-image""></i>"))
+                    Case ".png"
+                        iconCell.Controls.Add(New LiteralControl($"<i class=""bi bi-image""></i>"))
+                    Case Else
+                        iconCell.Controls.Add(New LiteralControl("<i class=""bi bi-file-earmark-text-fill""></i>"))
+                End Select
+
+                If fileName.Length > 32 Then
+                    fileName = fileName.Substring(Math.Max(fileName.Length - 32, 0))
+                End If
+
+                nameCell.Text = "..." & fileName
             End If
-            nameCell.Text = "..." & fileName
-        End If
 
+        Catch ex As Exception
+            Dim msg = "Error inesperado : " & ex.Message
+            RaiseEvent AlertGenerated(Me, New AlertEventArgs(msg, "danger"))
+        End Try
 
     End Sub
-
 
     Public Event AlertGenerated As EventHandler(Of AlertEventArgs)
 
