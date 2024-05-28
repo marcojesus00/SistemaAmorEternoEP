c9e2adc2f53aff911be5f70da8b60d499262e0f7
3GATO3 <106358166+Wilbert-Miau@users.noreply.github.com>
2024-05-28 10:43:39 -0600

    fix: not caching non existent images

--- Sistema/helpers/NumberHelper.vb
+++ Sistema/helpers/NumberHelper.vb
@@ -1,17 +1,23 @@
 Public Class NumberHelper
     Public Shared Function GenerateRandomNumber(n As Integer) As Integer
+        ' Input validation
         If n <= 0 Then
-            Throw New ArgumentOutOfRangeException("n", "Solo se aceptan números positivos en este número aleatorio.")
+            Throw New ArgumentOutOfRangeException("n", "Number of digits must be positive.")
         End If
+
+        ' Create a new Random object for better isolation
+        Dim randomizer As New Random()
 
         ' Calculate the lower and upper bounds for the random number
         Dim lowerBound As Integer = 10 ^ (n - 1)
         Dim upperBound As Integer = (10 ^ n) - 1
 
-        ' Use the Rnd function to generate a random number within the bounds
-        Dim randomNumber As Integer = Int(Rnd() * (upperBound - lowerBound + 1)) + lowerBound
+        ' Generate a random number within the bounds using Next
+        Dim randomNumber As Integer = randomizer.Next(lowerBound, upperBound + 1)
 
+        ' Return the random number
         Return randomNumber
     End Function
+
 
 End Class
