069487e5bb1cdb9b82007b872ba3be20f4b115ac
Wilbert-Miau <106358166+Wilbert-Miau@users.noreply.github.com>
2024-06-05 08:01:01 -0600

    feat: configuring map

---
+++ Sistema/Dashboards/CobrosMap.aspx.vb
@@ -0,0 +1,21 @@
+﻿Public Class CobrosMap
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            Dim script As String = "
+            <script>
+                var map = L.map('mapContainer').setView([15.400, -87.80], 13);
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: 'Map data © <a href=""https://openstreetmap.org"">OpenStreetMap</a> contributors',
+                    maxZoom: 18,
+                }).addTo(map);
+  var marker = L.marker([15.400, -87.80]).addTo(map);
+  marker.bindPopup(""<b>Hello world!</b><br />This is a popup."").openPopup();
+            </script>"
+            ClientScript.RegisterStartupScript(Me.GetType(), "initializeMap", script)
+        End If
+    End Sub
+
+
+End Class
