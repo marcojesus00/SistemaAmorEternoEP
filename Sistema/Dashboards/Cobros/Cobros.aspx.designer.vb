11bad6dccb69ca8e6a1be3115fa5b53a5389c271
Wilbert Castillo <106358166+Wilbert-Miau@users.noreply.github.com>
2024-06-17 19:21:55 -0600

    Revert "feat: show details"
    This reverts commit d514c3fe88ff8514703ca93606086d57d1f7c4e0.

--- Sistema/Dashboards/Cobros/Cobros.aspx.designer.vb
+++ Sistema/Dashboards/Cobros/Cobros.aspx.designer.vb
@@ -131,13 +131,13 @@ Partial Public Class CobrosDashboard
     Protected WithEvents DashboardGridview As Global.System.Web.UI.WebControls.GridView
 
     '''<summary>
-    '''DetailsControl control.
+    '''DetailsView1 control.
     '''</summary>
     '''<remarks>
     '''Auto-generated field.
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
-    Protected WithEvents DetailsControl As Global.System.Web.UI.WebControls.GridView
+    Protected WithEvents DetailsView1 As Global.System.Web.UI.WebControls.DetailsView
 
     '''<summary>
     '''alertPlaceholder control.
