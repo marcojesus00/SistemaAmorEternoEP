dcf99e9a7a408044ded02f60765ba1ffaa01bfad
Wilbert Castillo <106358166+Wilbert-Miau@users.noreply.github.com>
2024-12-17 14:31:13 -0600

    feat: send signal for account statments

--- Sistema/Helpers/RabbitMQ.Helper.vb
+++ Sistema/Helpers/RabbitMQ.Helper.vb
@@ -3,6 +3,11 @@ Imports System.Text
 Imports Newtonsoft.Json
 Public Class RabbitMQHelper
 
+    Private Shared ReadOnly Factory As New ConnectionFactory() With {
+        .HostName = "192.168.20.111", ' Change to your RabbitMQ host
+        .UserName = "guest",          ' Change to your RabbitMQ username
+        .Password = "Eterno.2020"     ' Change to your RabbitMQ password
+    }
     Public Shared Sub PublishToRabbitMQ(messageType As String, collectorId As String, userId As String, queueName As String)
         Dim factory As New ConnectionFactory() With {
             .HostName = "192.168.20.111", ' Change to your RabbitMQ host
@@ -22,6 +27,30 @@ Public Class RabbitMQHelper
                 }
 
                 Dim messageBody As String = JsonConvert.SerializeObject(message)
+                Dim body As Byte() = Encoding.UTF8.GetBytes(messageBody)
+
+                Dim properties = channel.CreateBasicProperties()
+                properties.Persistent = True
+
+                channel.BasicPublish(exchange:="", routingKey:=queueName, basicProperties:=properties, body:=body)
+            End Using
+        End Using
+    End Sub
+
+
+    Public Shared Sub PublishToRabbitMQ2(queueName As String, messageFields As Dictionary(Of String, String))
+        Using connection As IConnection = Factory.CreateConnection()
+            Using channel As IModel = connection.CreateModel()
+                ' Declare queue
+                channel.QueueDeclare(queue:=queueName, durable:=True, exclusive:=False, autoDelete:=False, arguments:=Nothing)
+
+                ' Prepare message
+                'Dim message As New Dictionary(Of String, Object)(additionalFields) From {
+                '    {"Timestamp", DateTime.Now()}
+                '}
+
+                ' Serialize and publish message
+                Dim messageBody As String = JsonConvert.SerializeObject(messageFields)
                 Dim body As Byte() = Encoding.UTF8.GetBytes(messageBody)
 
                 Dim properties = channel.CreateBasicProperties()
